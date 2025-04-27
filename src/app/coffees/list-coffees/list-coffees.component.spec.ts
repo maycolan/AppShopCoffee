6bc3bcca36c1cdd931e0675f8874d7f6ece997c9
maycolan <m.avendanon@uniandes.edu.co>
2025-04-27 15:41:58 -0500

    Punto 3: se realiza la prueba del componente. Para esto, en la prueba se crea un listado de 3 cafes usando faker y se verifica que la tabla se cree correctamente con tres filas mas el encabezado

--- src/app/coffees/list-coffees/list-coffees.component.spec.ts
+++ src/app/coffees/list-coffees/list-coffees.component.spec.ts
@@ -5,10 +5,13 @@ import { DebugElement } from '@angular/core';
 
 import { ListCoffeesComponent } from './list-coffees.component';
 import { provideHttpClient } from '@angular/common/http';
+import { Coffee } from '../coffee';
+import { faker } from '@faker-js/faker';
 
 describe('ListCoffeesComponent', () => {
   let component: ListCoffeesComponent;
   let fixture: ComponentFixture<ListCoffeesComponent>;
+  let debug: DebugElement;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -21,10 +24,36 @@ describe('ListCoffeesComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ListCoffeesComponent);
     component = fixture.componentInstance;
+    for(let i = 0; i < 3; i++) {
+      const coffee = new Coffee(
+        faker.number.int({ min: 1, max: 10 }), //id
+        faker.commerce.productName(), // nombre
+        faker.commerce.productMaterial(), // tipo
+        [faker.location.country(), faker.location.country()], // región 
+        [faker.commerce.productAdjective(), faker.commerce.productAdjective()], // sabor
+        faker.number.int({ min: 800, max: 2500 }), // altura
+        faker.image.urlPicsumPhotos() // imagen 
+      );
+      component.coffes.push(coffee);
+
+                                }
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("Component has a table", () => {
+    expect(debug.query(By.css("tbody")).childNodes.length).toBeGreaterThan(0);
+  });
+
+  it("Component has a table with 3 rows", () => {
+    expect(debug.queryAll(By.css(".rd"))).toHaveSize(3);
+  })
+
+  it("Component has a table with 1 header", () => {
+    expect(debug.queryAll(By.css("thead"))).toHaveSize(1);
+  })
 });
